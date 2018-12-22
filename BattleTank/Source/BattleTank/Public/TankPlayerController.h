d631ebf0540febca232f4f35802ee3f79feaf671
Dionne Pasion <allure.success@gmail.com>
2018-12-22 03:25:23 -0600

    Added ViewPortSize and HitLocation
    ViewPortSize gets the size of the screen,
    HitLocation works with the ViewPortSize and returns where the white dot is in our worl.

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -1,8 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
+#define OUT 
 
 #include "Tank.h"
+#include "Engine/World.h"
+#include "Engine/Classes/Kismet/GameplayStatics.h"
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
@@ -14,10 +17,21 @@
 UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
+
 	GENERATED_BODY()
+private:
+	UPROPERTY(EditAnywhere)
+		float CrossHairX = 0.5f;
+	UPROPERTY(EditAnywhere)
+		float CrossHairY = 0.33f;
+
 
 public:
-	ATank* GetControlledBattleTank() const;
 	void BeginPlay() override;
 	void Tick(float DeltaTime) override;
+	ATank* GetControlledBattleTank() const;
+	void AimTowardsCrosshair();
+	bool GetSightRayHitLocation(FVector& HitLocation);
+	bool LookDirection(const FVector& ScreenSize, FVector &WDir);
+	void GetViewportSize(int32&, int32&);
 };
