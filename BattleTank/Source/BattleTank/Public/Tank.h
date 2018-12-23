8a97ecca65113ca0052eb2cf54c99607cedd6e98
Dionne Pasion <allure.success@gmail.com>
2018-12-23 12:09:41 -0600

    Delegated Aiming Component

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
+#include "TankAimingComponent.h"
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
@@ -19,6 +20,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UTankAimingComponent* TankAimingComponent = nullptr;
+	
 
 public:	
 	// Called every frame
